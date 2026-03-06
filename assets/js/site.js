80021b2c9cc926e6a6a1fb0d0fc121e09b10b51f
VitaliiMR <vimo8@elev.techcollege.dk>
2026-03-06 09:19:46 +0100

    works

--- assets/js/site.js
+++ assets/js/site.js
@@ -4,6 +4,30 @@ let mySum = numberOne + numberTwo;
 return mySum;
 }
 
+function subtractNumbers(numberOne, numberTwo) {
+
+let myDifference = numberOne - numberTwo;
+return myDifference;
+}
+
+function multiplyNumbers(numberOne, numberTwo) {
+
+let myProduct = numberOne * numberTwo;
+return myProduct;
+}
+
+function divideNumbers(numberOne, numberTwo) {
+
+let myQuotient = numberOne / numberTwo;
+return myQuotient;
+}   
+
 let sum = addNumbers(5, 10);
+let difference = subtractNumbers(10, 5);
+let product = multiplyNumbers(5, 10);
+let quotient = divideNumbers(10, 5);
 
 console.log(sum);
+console.log(difference);
+console.log(product);
+console.log(quotient);
