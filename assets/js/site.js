596961421d6c152aa4cf0de32d99e47aa19ecf86
VitaliiMR <vimo8@elev.techcollege.dk>
2026-03-06 09:40:32 +0100

    Update site.js

--- assets/js/site.js
+++ assets/js/site.js
@@ -22,12 +22,20 @@ let myQuotient = numberOne / numberTwo;
 return myQuotient;
 }   
 
-let sum = addNumbers(5, 10);
-let difference = subtractNumbers(10, 5);
-let product = multiplyNumbers(5, 10);
-let quotient = divideNumbers(10, 5);
 
-console.log(sum);
-console.log(difference);
-console.log(product);
-console.log(quotient);
+// let sum = addNumbers(5, 10);
+// let difference = subtractNumbers(10, 5);
+// let product = multiplyNumbers(5, 10);
+// let quotient = divideNumbers(10, 5);
+
+// console.log(sum);
+// console.log(difference);
+// console.log(product);
+// console.log(quotient);
+
+
+const myButton = document.getElementById("button");
+myButton.addEventListener("click",function() {
+alert("You clicked the button!");
+
+})
